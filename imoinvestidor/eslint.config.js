794d8152e54e9fb96a49b72162fde0267712a618
Daniel R. <danielconceicaorodrigues23@gmail.com>
2025-05-06 21:52:12 +0100

    updates

--- imoinvestidor/eslint.config.js
+++ imoinvestidor/eslint.config.js
@@ -4,10 +4,9 @@ import pluginReact from "eslint-plugin-react";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/jsx-runtime"] },
   { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
 ]);
